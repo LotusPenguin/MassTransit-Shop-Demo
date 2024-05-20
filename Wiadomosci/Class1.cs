58f0e5f0bc3dbd2cc7688cc8f9a41eba7db00dbf
LotusPenguin <125930228+LotusPenguin@users.noreply.github.com>
2024-05-20 04:35:37 +0200

    Initial commit

---
+++ Wiadomosci/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wiadomosci
+{
+    public interface IQuantity
+    {
+        int ilosc { get; set; }
+    }
+
+    public interface IStartZamówienia : IQuantity { }
+    public interface IPytanieOPotwierdzenie : IQuantity { }
+    public interface IPotwierdzenie { }
+    public interface IBrakPotwierdzenia { }
+    public interface IPytanieoWolne : IQuantity { }
+    public interface OdpowiedzWolneNegatywna { }
+    public interface AkceptacjaZamówienia : IQuantity { }
+    public interface OdrzucenieZamówienia : IQuantity { }
+}
